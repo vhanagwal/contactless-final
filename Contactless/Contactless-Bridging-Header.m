4505df9c860cc9375b0f47a496c96f2386b484ee
Vardhan Agrawal <30162871+vhanagwal@users.noreply.github.com>
2017-09-01 23:36:50 -0700

    first commit

---
+++ Contactless/Contactless-Bridging-Header.m
@@ -0,0 +1,9 @@
+//
+//  Contactless-Bridging-Header.m
+//  Contactless
+//
+//  Created by Vardhan Agrawal on 8/31/17.
+//  Copyright © 2017 Vardhan Agrawal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Contactless/TableState.swift
@@ -0,0 +1,9 @@
+//
+//  TableState.swift
+//  Contactless
+//
+//  Created by Vardhan Agrawal on 9/1/17.
+//  Copyright © 2017 Vardhan Agrawal. All rights reserved.
+//
+
+import Foundation
